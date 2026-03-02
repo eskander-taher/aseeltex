a342051fa1389f354f66376376b3326df68edc11
eskander-taher <eskander.alshaibany@gmail.com>
2026-03-03 00:33:44 +0300

    add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,13 +12,31 @@ const cairo = Cairo({
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: "اسيلتكس للسفر والسياحة | اسيلتكس Travel & Tourism",
-    template: "%s | اسيلتكس للسفر والسياحة",
-  },
-  description:
-    "شركة اسيلتكس للسفر والسياحة - بكجات سياحية لروسيا، حجوزات فنادق، جولات سياحية، استقبال وتوديع",
-  keywords: ["سياحة روسيا", "اسيلتكس", "سفر", "حجوزات فنادق", "جولات سياحية", "اسيلتكس"],
+	title: {
+		default: "اسيلتكس للسفر والسياحة | اسيلتكس Travel & Tourism",
+		template: "%s | اسيلتكس للسفر والسياحة",
+	},
+	description:
+		"شركة اسيلتكس للسفر والسياحة - بكجات سياحية لروسيا، حجوزات فنادق، جولات سياحية، استقبال وتوديع",
+	keywords: ["سياحة روسيا", "اسيلتكس", "سفر", "حجوزات فنادق", "جولات سياحية", "اسيلتكس"],
+	icons: {
+		icon: [
+			{ url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+			{ url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+		],
+		apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+		other: [
+			{
+				rel: "android-chrome",
+				url: "/android-chrome-192x192.png",
+			},
+			{
+				rel: "android-chrome",
+				url: "/android-chrome-512x512.png",
+			},
+		],
+	},
+	manifest: "/site.webmanifest",
 };
 
 export const viewport: Viewport = {
